1c2af2a9c2ec984053d5a12793d9f7f34390ce09
Kieran Murphy <kmurphy37@luc.edu>
2021-11-05 08:46:29 -0500

    implements Merge Sort

--- lib/src/main/java/comp271/sorting/MergeSortPriorityQueue.java
+++ lib/src/main/java/comp271/sorting/MergeSortPriorityQueue.java
@@ -28,7 +28,55 @@ public class MergeSortPriorityQueue implements PriorityQueue {
 
     private void sort(Integer[] numbers) {
 
-        // implement with merge sort algorithm
-        throw new UnsupportedOperationException("not implemented yet!");
+        mergeSort(numbers, 0, numbers.length-1);
+    }
+
+    private void mergeSort(Integer[] numbers, int startIndex, int endIndex) {
+        int midPoint = 0;
+
+        if (startIndex < endIndex) {
+            midPoint = (startIndex + endIndex)/2;
+
+            mergeSort(numbers, startIndex, midPoint);
+            mergeSort(numbers, midPoint+1, endIndex);
+
+            merge(numbers, startIndex, midPoint, endIndex);
+        }
+    }
+
+    private void merge(Integer[] numbers, int startIndex, int midPoint, int endIndex) {
+
+        int sizeOfMergedNumbers = endIndex - startIndex + 1;
+        int leftPosition = startIndex;
+        int rightPosition = midPoint + 1;
+        int mergePosition = 0;
+        Integer[] mergedNumbers = new Integer[sizeOfMergedNumbers];
+
+        while (leftPosition <= midPoint && rightPosition <= endIndex) {
+            if (numbers[leftPosition] < numbers[rightPosition]) {
+                mergedNumbers[mergePosition] = numbers[leftPosition];
+                leftPosition++;
+            } else {
+                mergedNumbers[mergePosition] = numbers[rightPosition];
+                rightPosition++;
+            }
+            mergePosition++;
+        }
+
+        while (leftPosition <= midPoint) {
+            mergedNumbers[mergePosition] = numbers[leftPosition];
+            leftPosition++;
+            mergePosition++;
+        }
+
+        while (rightPosition <= endIndex) {
+            mergedNumbers[mergePosition] = numbers[rightPosition];
+            rightPosition++;
+            mergePosition++;
+        }
+
+        for (mergePosition = 0; mergePosition < sizeOfMergedNumbers; mergePosition++) {
+            numbers[startIndex + mergePosition] = mergedNumbers[mergePosition];
+        }
     }
 }
